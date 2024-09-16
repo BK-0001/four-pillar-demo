f89952f174b9994c4c8ed04211087054aeb54860
Bart Kang <bart.kang.0719@gmail.com>
2024-09-16 14:09:27 -0700

    demo abstract method

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ class Course {
       throw new Error("Abstract class should not be instantiated directly");
     }
   }
+
+  takeStudent() {
+    throw new Error("abstract methods must be implemented");
+  }
 }
 
-const oop = new Course();
-console.log(oop);
+class OOP extends Course {
+  takeStudent() {
+    console.log(
+      "taking students and students must complete some courses blah blah blah........."
+    );
+  }
+}
+
+const oop = new OOP();
+console.log(oop.takeStudent());
